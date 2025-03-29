aabde4162088a3167d647113486be8d17ea5556d
FindTheBugg <vadim2012-2013@mail.ru>
2025-03-29 11:38:42 +0300

    added file project

---
+++ testApp/testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by vadim on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
